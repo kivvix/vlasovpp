cfadacefc97e7c2a6e19ab2dee5f96f8abab1e99
Josselin <josselin.massot@gmail.com>
2021-07-08 11:53:41 +0200

    🎨 improve argument possibility

--- script/entro.py
+++ script/entro.py
@@ -10,14 +10,15 @@ def params ( s ):
     cmd.append('-m')
   meth = s[int(b)]
   cmd.append("-{}".format(meth))
-  n = int(s[int(b)+1])
-  m = " "
-  if meth == 'p':
-    try:
-      m = int(s[int(b)+2])
-    except ValueError:
-      m = n
-  cmd.extend([str(n),str(m)])
+  if meth == 'p' or meth == 't' :
+    n = int(s[int(b)+1])
+    m = " "
+    if meth == 'p':
+      try:
+        m = int(s[int(b)+2])
+      except ValueError:
+        m = n
+    cmd.extend([str(n),str(m)])
   rk = s[-4:]
   cmd.append("--lrk={}".format(rk.upper()))
 
@@ -29,13 +30,13 @@ def worker ( s ):
   cmd.extend("-o ../code/mtest".split())
   command_line = " ".join(cmd)
   print(command_line)
-  p = subprocess.Popen(command_line.split())
-  p.wait()
+  #p = subprocess.Popen(command_line.split())
+  #p.wait()
 
 num = 1
 tp = ThreadPool(num)
-#for s in "p22rk44 t5rk44 mp22rk44 mt5rk44 mp21rk33 mp12rk33 mp11rk33 mp22rk33 mt1rk33 mt2rk33 mt3rk33 mt4rk33 mp22dp43 mt5dp43".split():
-for s in "mp22dp43 mt5dp43".split():
+#for s in "mp22dp43 mt5dp43".split():
+for s in "erk44 p22rk44 t5rk44 mp22rk44 mt5rk44 mp21rk33 mp12rk33 mp11rk33 mp22rk33 mt1rk33 mt2rk33 mt3rk33 mt4rk33 mp22dp43 mt5dp43".split():
   tp.apply_async( worker , (s,) )
 
 tp.close()
